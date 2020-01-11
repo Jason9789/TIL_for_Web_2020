d4a8eeca59eff78803a9347420d13608880d7b66
PanGeun <lifebook0809@gmail.com>
2020-01-11 18:46:30 +0900

    make front-end

--- Node/nodebird/app.js
+++ Node/nodebird/app.js
@@ -1,4 +1,4 @@
-const express = require("expree");
+const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const path = require("path");
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.set("port", process.env.PORT || 8001);
 
-app.use(morga("div"));
+app.use(morgan("div"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
