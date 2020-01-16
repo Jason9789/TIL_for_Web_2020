ccb6894d34dd086432a7d51e28809b7dd9e0e391
PanGeun <lifebook0809@gmail.com>
2020-01-16 20:28:41 +0900

    Connect Nodejs and MySQL

--- Node/CRUD_practice/crud_practice/models/index.js
+++ Node/CRUD_practice/crud_practice/models/index.js
@@ -17,4 +17,10 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
+db.User = require("./user")(sequelize, Sequelize);
+db.Comment = require("./comment")(sequelize, Sequelize);
+
+db.User.hasMany(db.Comment, { foreignKey: "commenter", sourceKey: "id" });
+db.Comment.belongsTo(db.User, { foreignKey: "commenter", targetKey: "id" });
+
 module.exports = db;
