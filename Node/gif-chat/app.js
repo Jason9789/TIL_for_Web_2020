825be498ea5b5f710927cfbefb58d7131b5af181
PanGeun <lifebook0809@gmail.com>
2020-01-17 16:23:45 +0900

    use Web Socket with ws module

--- Node/gif-chat/app.js
+++ Node/gif-chat/app.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
   res.render("error");
 });
 
-app.listen(app.get("port"), () => {
+const server = app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기 중");
 });
 
