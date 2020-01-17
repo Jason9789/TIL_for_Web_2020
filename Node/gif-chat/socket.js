cc840ced2a105c91985a3b485c842dff859b0342
PanGeun <lifebook0809@gmail.com>
2020-01-17 17:03:54 +0900

    use Socket.IO

--- Node/gif-chat/socket.js
+++ Node/gif-chat/socket.js
@@ -1,30 +1,54 @@
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
+const SocketIO = require("socket.io");
 
 module.exports = server => {
-  const wss = new WebSocket.Server({ server });
+  // ws 사용
 
-  wss.on("connection", (ws, req) => {
-    // 클라이언트의 ip를 알아내는 유명한 방법 중 하나.
+  //   const wss = new WebSocket.Server({ server });
+  //   wss.on("connection", (ws, req) => {
+  //     // 클라이언트의 ip를 알아내는 유명한 방법 중 하나.
+  //     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+  //     console.log("새로운 클라이언트 접속", ip);
+
+  //     ws.on("message", message => {
+  //       console.log(message);
+  //     });
+  //     ws.on("error", error => {
+  //       console.error(error);
+  //     });
+
+  //     ws.on("close", () => {
+  //       console.log("클라이언트 접속 해제", ip);
+  //       clearInterval(ws.interval);
+  //     });
+
+  //     const interval = setInterval(() => {
+  //       if (ws.readyState === ws.OPEN) {
+  //         ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
+  //       }
+  //     }, 3000);
+  //     ws.interval = interval;
+  //   });
+
+  // Socket.io 사용
+  const io = SocketIO(server, { path: "/socket.io" });
+
+  io.on("connection", socket => {
+    const req = socket.request;
     const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
-    console.log("새로운 클라이언트 접속", ip);
+    console.log("새로운 클라이언트 접속! ", ip, socket.id, req, ip);
 
-    ws.on("message", message => {
-      console.log(message);
-    });
-    ws.on("error", error => {
-      console.error(error);
+    socket.on("disconnect", () => {
+      console.log("클라이언트 접속 해제", ip, socket.id);
+      clearInterval(socket.interval);
     });
 
-    ws.on("close", () => {
-      console.log("클라이언트 접속 해제", ip);
-      clearInterval(ws.interval);
-    });
-
-    const interval = setInterval(() => {
-      if (ws.readyState === ws.OPEN) {
-        ws.send("서버에서 클라이언트로 메시지를 보냅니다.");
-      }
-    }, 3000);
-    ws.interval = interval;
+    socket.on(
+      "reply",
+      data => {
+        socket.emit("news", "Hello Socket.IO");
+      },
+      3000
+    );
   });
 };
