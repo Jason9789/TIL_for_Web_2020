f6aaa67d76aec2e292f5c42d4085a6a1bc87864b
PanGeun <lifebook0809@gmail.com>
2020-01-18 18:53:12 +0900

    add socket

--- Node/gif-chat/app.js
+++ Node/gif-chat/app.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
+const ColorHash = require("color-hash");
 require("dotenv").config();
 
 const webSocket = require("./socket");
@@ -13,27 +14,36 @@ const connect = require("./schemas");
 const app = express();
 connect();
 
+const sessionMiddleware = session({
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.set("port", process.env.PORT || 8005);
 
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/gif", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-      httpOnly: true,
-      secure: false
-    }
-  })
-);
+app.use(sessionMiddleware);
 app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.color) {
+    const colorHash = new ColorHash();
+    req.session.color = colorHash.hex(req.sessionID);
+  }
+  next();
+});
 
 app.use("/", indexRouter);
 
@@ -51,7 +61,7 @@ app.use((err, req, res, next) => {
 });
 
 const server = app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
 
-webSocket(server);
+webSocket(server, app, sessionMiddleware);
