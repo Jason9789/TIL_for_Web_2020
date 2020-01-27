1c75ba5f797fd6947eb3b5be6d364f8f56aa374e
PanGeun <lifebook0809@gmail.com>
2020-01-27 19:27:01 +0900

    study react + express

--- Node/r_e_practice/test-backend/client/src/components/Header.js
+++ Node/r_e_practice/test-backend/client/src/components/Header.js
@@ -1,24 +1,36 @@
 import React from "react";
 import { Link } from "react-router";
+import PropTypes from "prop-types";
 import "./Header.css";
 
 const MenuItem = ({ active, children, to }) => (
-  <Link to={to} className="menu-item">
+  <Link to={to} className={`menu-item ${active ? "active" : ""}`}>
     {children}
   </Link>
 );
 
-const Header = () => {
+const Header = (props, context) => {
+  const { router } = context;
   return (
     <div>
-      <div className="logo">PanGeun</div>
+      <div className="logo">velopert</div>
       <div className="menu">
-        <MenuItem to={"/"}>홈</MenuItem>
-        <MenuItem to={"/about"}>소개</MenuItem>
-        <MenuItem to={"/post"}>포스트</MenuItem>
+        <MenuItem to={"/"} active={router.isActive("/", true)}>
+          홈
+        </MenuItem>
+        <MenuItem to={"/about"} active={router.isActive("/about")}>
+          소개
+        </MenuItem>
+        <MenuItem to={"/post"} active={router.isActive("/post")}>
+          포스트
+        </MenuItem>
       </div>
     </div>
   );
+};
+
+Header.contextTypes = {
+  router: PropTypes.object
 };
 
 export default Header;
